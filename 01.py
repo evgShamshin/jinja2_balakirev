9ad29a4472c857a122c695981e5038215226efdc
evg_shamsin <i@eshamshin.ru>
2025-03-08 00:36:31 +0300

    2025.03.07

---
+++ 01.py
@@ -0,0 +1,8 @@
+from jinja2 import Template
+
+name = 'user_name'
+surname = 'user_surname'
+
+tm = Template('Hello {{name.upper()}} {{surname.lower()}}!')
+msg = tm.render(name=name, surname=surname)
+print(msg)
